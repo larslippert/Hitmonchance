f8fcdaaa0225a7595efeaa5e298e89b628974b0b
Lars Lippert Ovesen <lars_lippert@hotmail.com>
2019-04-30 17:20:10 +0200

    Test

--- app/src/main/java/com/lalov/hitmonchance/LoginActivity.java
+++ app/src/main/java/com/lalov/hitmonchance/LoginActivity.java
@@ -6,19 +6,21 @@ import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-/**
+
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
+//import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
- */
+
 
 public class LoginActivity extends AppCompatActivity {
 
-    EditText textEmail, textPassword;
-    TextView textLogin;
+    EditText txtEmail, txtPassword;
+    TextView txtLogin;
     Button btnForgotPassword, btnSignUp, btnSignIn;
 
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +29,13 @@ public class LoginActivity extends AppCompatActivity {
         getSupportActionBar().hide(); //hide the title bar
         setContentView(R.layout.activity_login);
 
+        txtEmail = findViewById(R.id.editTextEmail);
+        txtPassword = findViewById(R.id.editTextPassword);
+        txtLogin = findViewById(R.id.txtLogin);
+        btnForgotPassword = findViewById(R.id.btnForgot);
+        btnSignIn = findViewById(R.id.btnSignIn);
+        btnSignUp = findViewById(R.id.btnSignUp);
 
+        
     }
 }
