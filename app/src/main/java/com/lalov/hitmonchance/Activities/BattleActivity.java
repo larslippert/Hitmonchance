17b301e0f4e86256e45f4c4d66fcf530f55bf712
Christian Thing Knudsen <ct-1000@hotmail.com>
2019-05-09 13:43:53 +0200

    Manifest fix

--- app/src/main/java/com/lalov/hitmonchance/Activities/BattleActivity.java
+++ app/src/main/java/com/lalov/hitmonchance/Activities/BattleActivity.java
@@ -1,7 +1,9 @@
-package com.lalov.hitmonchance;
+package com.lalov.hitmonchance.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.lalov.hitmonchance.R;
 
 public class BattleActivity extends AppCompatActivity {
 
